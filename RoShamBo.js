4b46e8bfce54a3facb7cf27ea9c4f93bf33b142e
Cucius <Rylan19@Gmail.com>
2022-08-06 02:01:05 -0600

    Refactored code and added function to play again

--- RoShamBo.js
+++ RoShamBo.js
@@ -1,78 +1,77 @@
-
-//starting prompt
-var userWeapon = prompt("Please choose Shield(R) Sword(P) or Bow(S)");
-
-//user input of Shield, sword or Bow
-if (userWeapon === "Shield" || userWeapon == "r" || userWeapon == "R") {
-    userWeapon = "Shield";
-    console.log("Shield");
-}else if (userWeapon === "Sword" || userWeapon == "p" || userWeapon == "P") {
-    userWeapon = "Sword";
-    console.log("Sword");
-}else if (userWeapon === "Bow" || userWeapon == "s" || userWeapon == "S") {
-    userWeapon = "Bow";
-    console.log("Bow");
-}else {
-    confirm("Coward!!! You have fled.");
-
-}
-
-//Computer random choice of Shield, Sword or Bow
-var trainingDummy = ["Shield","Sword","Bow"];
-
-var smartChoice = trainingDummy [Math.floor(Math.random()*trainingDummy.length)];
-console.log(smartChoice);
-
-alert("Training Dummy has chosen " + smartChoice + "!" );
-
-//Possible results
-var battleResult = ["You are Victorious!", "You have been Defeated!", "Losses on both sides, Draw!"];
-
-//Result of the battle with computer as Bow
-if(userWeapon === "Shield" && smartChoice === "Bow" ){
-    alert(battleResult[0]);
-} else if (userWeapon === "Sword" && smartChoice === "Bow" ){
-    alert(battleResult[1]);
-}
-
-//Result of the battle with computer as Shield
-else if(userWeapon === "Sword" && smartChoice === "Shield" ){
-    alert(battleResult[0]);
-} else if (userWeapon === "Bow" && smartChoice === "Shield" ){
-    alert(battleResult[1]);
-}
-//Result of the battle with computer as Sword
-else if(userWeapon === "Bow" && smartChoice === "Sword" ){
-    alert(battleResult[0]);
-
-} else if (userWeapon === "Shield" && smartChoice === "Sword" ){
-    alert(battleResult[1]);
-}
-else if (userWeapon === smartChoice){
-    alert(battleResult[2]);
-}
-
-
-    var playAgain = confirm("Play again? ");
-
-    
-    if (playAgain) {
-        battle();
-    } else{
-    alert("Fair thee well");
-    }
-
-
-
-
-
 var wins = 0;
 
 var losses = 0;
 
 var draw = 0;
 
-alert("Wins: " + w +" \nLosses: " + losses + "\nDraws: " + draw);
-// console.log ("linked")
+battle = function () {
+  //starting prompt
+  var userWeapon = prompt("Please choose Shield(R) Dagger(P) or Bow(S)").toLowerCase();
 
-battle()
+  //user input of Shield, Dagger or Bow
+  if (userWeapon === "shield" || userWeapon == "r") {
+    userWeapon = "Shield";
+    console.log("Shield");
+  } else if (userWeapon === "Dagger" || userWeapon == "p") {
+    userWeapon = "Dagger";
+    console.log("Dagger");
+  } else if (userWeapon === "bow" || userWeapon == "s") {
+    userWeapon = "Bow";
+    console.log("Bow");
+  } else {
+    confirm("Coward!!! You have fled.");
+  }
+
+  //Computer random choice of Shield, Dagger or Bow
+  var trainingDummy = ["Shield", "Dagger", "Bow"];
+
+  var smartChoice = trainingDummy[Math.floor(Math.random() * trainingDummy.length)];
+  console.log(smartChoice);
+
+  alert("Training Dummy has chosen " + smartChoice + "!");
+
+  //Possible results
+  var battleResult = ["You are Victorious!", "You have been Defeated!", "Losses on both sides, Draw!"];
+
+  //Result of the battle with computer as Bow
+  if (userWeapon === "Shield" && smartChoice === "Bow") {
+    alert(battleResult[0]);
+    wins++;
+  } else if (userWeapon === "Dagger" && smartChoice === "Bow") {
+    alert(battleResult[1]);
+    losses++;
+  }
+
+  //Result of the battle with computer as Shield
+  else if (userWeapon === "Dagger" && smartChoice === "Shield") {
+    alert(battleResult[0]);
+    wins++;
+  } else if (userWeapon === "Bow" && smartChoice === "Shield") {
+    alert(battleResult[1]);
+    losses++;
+  }
+  //Result of the battle with computer as Dagger
+  else if (userWeapon === "Bow" && smartChoice === "Dagger") {
+    alert(battleResult[0]);
+    wins++;
+  } else if (userWeapon === "Shield" && smartChoice === "Dagger") {
+    alert(battleResult[1]);
+    losses++;
+  } else if (userWeapon === smartChoice) {
+    alert(battleResult[2]);
+    draw++;
+  }
+
+  var playAgain = confirm("Play again? ");
+
+  if (playAgain) {
+    battle();
+  } else {
+    alert("Fair thee well");
+  }
+
+  return alert("Wins: " + wins + " \nLosses: " + losses + "\nDraws: " + draw);
+
+  // console.log ("linked")
+};
+battle();
